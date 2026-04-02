098de4a913ac7b53c60b32c3fb549d7e5d3e0b8b
MD Khalid Saifullah Seyam <198321769+seyam02@users.noreply.github.com>
2026-04-02 10:04:39 +0600

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,36 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<int>nge(vector<int> &arr){
-    int n = arr.size();
-    vector<int> output(n, -1);
-    stack<int> st;
-    st.push(0);
-    for(int i=1; i<n; i++){
-        while(!st.empty() && arr[i]>arr[st.top()]){
-            output[st.top()] = arr[i];
-            st.pop();
-        }
-        st.push(i);
-    }
-    while(! st.empty()){
-        output[st.top()]  = -1;
-        st.pop();
-    }
-    return output;
-}
-int main(){
-    int n;
-    cin>>n;
-    vector<int> v;
-    while(n--){
-        int x;
-        cin>>x;
-        v.push_back(x);
-    }
-    vector<int> res = nge(v);
-    for(int i=0; i<res.size(); i++){
-        cout<<res[i]<<" ";
+
+class Stack{
+    int arr[100];
+    int top;
+
+public:
+    Stack(){
+        top = -1;
     }
 
-    return 0;
+    void push(int x){
+        if(top == 99) return;
+        arr[++top] = x;
+    }
+
+    void pop(){
+        if(top == -1) return;
+        top--;
+    }
+
+    int peek(){
+        if(top == -1) return -1;
+        return arr[top];
+    }
+
+    bool isEmpty(){
+        return (top == -1);
+    }
+};
+
+int main(){
+    Stack s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    cout << s.peek() << endl;
+
+    s.pop();
+    cout << s.peek() << endl;
 }
