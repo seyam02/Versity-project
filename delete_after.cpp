7a8ddc653cdaf7d929b356aa9eb3127c63d82852
MD Khalid Saifullah Seyam <198321769+seyam02@users.noreply.github.com>
2026-04-29 23:51:47 +0600

    Fix formatting issues in delete_after.cpp

--- delete_after.cpp
+++ delete_after.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     int n = 5;
     int* a = new int[n];
-    for(int i=0;i<n;i++) cin >> a[i];
+    for(int i=0;i<n;i++) cin >> a[i]; 
 
     int pos;
     cin >> pos;
