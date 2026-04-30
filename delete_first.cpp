13069ccfb2ad77f3823645d47ea5e0e7cda8b39e
MD Khalid Saifullah Seyam <198321769+seyam02@users.noreply.github.com>
2026-04-30 19:52:20 +0600

    Correct memory allocation for deleting first element and
    Fix memory allocation for temp array in delete_first function.

--- delete_first.cpp
+++ delete_first.cpp
@@ -7,7 +7,8 @@ int main() {
     for(int i=0;i<n;i++) cin >> a[i];
 
     if(n > 0){
-        int* temp = new int[n-1];
+        int* temp = new int[n-1];  
+        
         for(int i=1;i<n;i++) temp[i-1] = a[i];
         delete[] a;
         a = temp;
